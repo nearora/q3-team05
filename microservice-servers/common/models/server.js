cb33b5030f0a53b687d4cf94328e3cdf21941379
Neeraj Arora <narora+github@narora.net>
2016-10-22 15:45:42 -0700

    Added ability to GET reservations and servers by id.

--- microservice-servers/common/models/server.js
+++ microservice-servers/common/models/server.js
@@ -2,24 +2,38 @@
 
 'use strict';
 
-const serverCreateRequestsQueue =
-	'http://localhost:8080/api/topic/server-create-requests';
+const serverCreateRequestsQueue = 'server-create-requests';
 
 var queueOperations = require('../queueOperations.js');
 var blobOperations = require('../blobOperations.js');
 
 module.exports = function(Server) {
 	Server.find = function(f, cb) {
-		console.log('Server.find called')
+		console.log('Server.find called');
 		// Get all servers from the blob service
 		var c = blobOperations.readFromBlob();
 		cb(null, c.servers);
-	}
+	};
 	
 	Server.create = function(d, cb) {
-		console.log('Server.create called')
+		console.log('Server.create called');
 		// Write a new server to the queue
 		queueOperations.writeToQueue(serverCreateRequestsQueue, d);
 		cb(null, d);
-	}
+	};
+	
+	Server.findById = function(id, f, cb) {
+		console.log('Server.findById	 called');
+		console.log('Searching for server with id ' + id);
+		// Get all servers from the blob service
+		var c = blobOperations.readFromBlob();
+		// Find the server in the array of servers
+		var r;
+		for (var i = 0; i < c.servers.length; i++) {
+			if (id == c.servers[i].id) {
+				r = c.servers[i];
+			}
+		}
+		cb(null, r);
+	};
 };
